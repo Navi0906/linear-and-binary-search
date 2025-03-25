7a6648f41cc5c645a81f5c7cd9a6a7131d14e59f
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-25 21:32:18 +0530

    Naam Simran Singh Add: Finding first and last occurence

---
+++ FirstLastOccurence.java
@@ -0,0 +1,43 @@
+public class FirstLastOccurence {
+    public static int findFirstOccurrence(int[] arr, int target) {
+        int left = 0, right = arr.length - 1, first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    public static int findLastOccurrence(int[] arr, int target) {
+        int left = 0, right = arr.length - 1, last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                last = mid;
+                left = mid + 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3, 4, 5};
+        int target = 2;
+        int first = findFirstOccurrence(arr, target);
+        int last = findLastOccurrence(arr, target);
+
+        System.out.println("First occurrence: " + first);
+        System.out.println("Last occurrence: " + last);
+    }
+}
